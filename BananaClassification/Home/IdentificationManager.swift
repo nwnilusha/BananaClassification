35ebbf0b6dbafe2cdde1331cfa0080d1f06e73ae
Nilusha Wimalasena <nw.nilusha@gmail.com>
2024-11-12 18:29:29 +0000

    Remove project files

--- BananaClassification/Home/IdentificationManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  IdentificationManager.swift
-//  BananaClassification
-//
-//  Created by Nilusha Niwanthaka Wimalasena on 7/11/24.
-//
-
-import Foundation
--- Pods/Target Support Files/Pods-BananaClassification-BananaClassificationUITests/Pods-BananaClassification-BananaClassificationUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BananaClassification_BananaClassificationUITests : NSObject
-@end
-@implementation PodsDummy_Pods_BananaClassification_BananaClassificationUITests
-@end
